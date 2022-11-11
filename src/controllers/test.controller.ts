35e38b668b0a3a9b7e2a1d6cebdabebb4e10c82d
unknown <francisco.lizana1801@alumnos.ubiobio.cl>
2022-11-11 10:53:38 -0300

    Test de conectividad con metodos POST para cliente HTTP

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 export const postTest = async (req: Request, res: Response)=>{
     try{
-        const {body} = req.body;
+        const body = req.body;
         res.status(200).json({
             message:"Test de metodos POST",
             data:body
